4e85967e0ef09da2d4976f48ab7228ad4a6cc22c
dmawlda <mayangaramirezemerson@gmail.com>
2026-04-30 21:51:07 -0500

    cambios antes de revert

--- src/p4/Main.java
+++ src/p4/Main.java
@@ -24,7 +24,7 @@ public class Main {
         System.out.println("Suma: " + suma);
         System.out.println("Multiplicación: " + multiplicacion);
         System.out.println("División: " + division);
-
+// cambios realizadfos
         sc.close();
     }
 }
