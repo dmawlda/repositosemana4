eaeed429a5623d8acff7fa10b17dec9982d79ce0
dmawlda <mayangaramirezemersonr@gmail.com>
2026-04-30 20:56:36 -0500

    Se agrego la funcion division

--- src/p4/Calculadora.java
+++ src/p4/Calculadora.java
@@ -7,4 +7,11 @@ public class Calculadora {
     public int multiplicar(int a, int b) {
         return a * b;
     }
+    public double dividir(int a, int b) {
+        if (b == 0) {
+            System.out.println("Error: no se puede dividir entre 0");
+            return 0;
+        }
+        return (double) a / b;
+    }
 }
--- src/p4/Main.java
+++ src/p4/Main.java
@@ -14,8 +14,10 @@ public class Main {
         int num2 = sc.nextInt();
         int suma = calc.sumar(num1, num2);
         int multiplicacion = calc.multiplicar(num1, num2);
+        double division = calc.dividir(num1, num2);
         System.out.println("Suma: " + suma);
         System.out.println("Multiplicación: " + multiplicacion);
+        System.out.println("División: " + division);
 
         sc.close();
     }
