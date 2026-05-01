bbd03585fcaa21ac7698677937b026e95aa23a63
dmawlda <mayangaramirezemerson@gmail.com>
2026-04-30 21:23:53 -0500

    Calculadora Finalizada

---
+++ src/p4/Calculadora.java
@@ -0,0 +1,10 @@
+package p4;
+
+public class Calculadora {
+    public int sumar(int a, int b) {
+        return a + b;
+    }
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+}
---
+++ src/p4/Main.java
@@ -0,0 +1,27 @@
+package p4;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Calculadora calc = new Calculadora();
+        System.out.print("Ingrese el primer número: ");
+        int num1 = sc.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int num2 = sc.nextInt();
+        int suma = calc.sumar(num1, num2);
+        int multiplicacion = calc.multiplicar(num1, num2);
+        System.out.println("Suma: " + suma);
+        System.out.println("Multiplicación: " + multiplicacion);
+<<<<<<< HEAD
+        System.out.println("División: " + division);
+// hola mundo
+=======
+
+>>>>>>> parent of eaeed42 (Se agrego la funcion division)
+        sc.close();
+    }
+}
